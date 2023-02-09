e6c26eac6367ea1f2113e5b981306c47f9952aed
nizhantiw <nizhantiw@gmail.com>
2023-02-09 14:06:28 +0530

    Update Row Index Identification.java

--- Row Index Identification.java
+++ Row Index Identification.java
@@ -1,34 +1,23 @@
-
-import java.io.*; // for handling input/output
-import java.util.*; // contains Collections framework
-
-// don't change the name of this class
-// you can add inner classes if needed
-class RowIndexIdentificationContest {
-    public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in);
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String line = br.readLine(); //"3 4"
-        String nums[] = line.split(" "); //["3","4"]
-        int n = Integer.parseInt(nums[0]);
-        int m = Integer.parseInt(nums[1]);
-        int row = 1;
-        long max = 0;
-        long arr[][] = new long[n][m];
-
-        for (int i = 0; i < n; i++) {
-            long sum = 0;
-            nums = br.readLine().split(" ");
-            for (int j = 0; j < m; j++) {
-                arr[i][j] = Integer.parseInt(nums[j]);
-                sum += arr[i][j];
-            }
-            if (sum > max) {
-                row = i + 1;
-                max = sum;
-            }
-        }
-        System.out.println(row);
-
+#include <bits/stdc++.h> // header file includes every Standard library
+using namespace std;
+#define ll long long
+int main() {
+	// Your code here
+    int n, m;
+    cin>> n>> m;
+    vector<vector <int>> mat(n, vector<int>(m));
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++)
+        cin>>mat[i][j];
     }
+    ll mx=0;
+    int mx_idx=0;
+    for(int i=0;i<n;i++){
+        ll sum= accumulate(mat[i].begin(), mat[i].end(),0LL);
+        if(sum>mx){
+            mx=sum;
+            mx_idx=i;
+        }
+    }
+    cout<<mx_idx+1;
 }
